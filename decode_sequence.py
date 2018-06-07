daea3b583796997c9137ad5929daaaa907316153
Alan Au <alan.au@gmail.com>
2018-06-06 18:41:58 -0700

    Adding better comments.

--- decode_sequence.py
+++ decode_sequence.py
@@ -37,41 +37,43 @@ def decode_sequence(my_str):
     results = []
     i = len(my_str)
     if i == 0: 
-        return results #empty case
+        return results #empty/base case
 
     #case 1, take char and prepend to all existing results
     new_chr = my_chr(my_str[0])
     if new_chr is not None:
         if i > 1:
-            if my_str[1] != '0':
-                old_combos = decode_sequence(my_str[1:])
+            if my_str[1] != '0': #ignore the number starts with "0" case
+                old_combos = decode_sequence(my_str[1:]) #call recursively if there's more stuff in sequence
                 for old_str in old_combos:
                     new_str = new_chr + old_str
                     results.append(new_str)
         else:
-            results.append(new_chr)
+            results.append(new_chr) #otherwise seed a new combination
     
-    #case 2
+    #case 2, take pair of numbers and prepend to all existing results
     if i > 1:
         new_pair = my_pair(my_str[0],my_str[1])
         if new_pair is not None:
-            if i > 2:
-                if my_str[2] != '0':
-                    old_combos = decode_sequence(my_str[2:])
+            if i > 2: 
+                if my_str[2] != '0': #ignore the number starts with "0" case
+                    old_combos = decode_sequence(my_str[2:]) #call recursively if there's more stuff in sequence
                     for old_str in old_combos:
                         new_str = new_pair + old_str
                         results.append(new_str)
-            else:
-                results.append(new_pair)
+            else: 
+                results.append(new_pair) #otherwise seed a new combination
     return results
 
 #standalone execution
 if __name__ == "__main__":
-    example = input("Please enter a sequence of numbers: ").strip()
+    #get input here, do some format checking
+    example = input("Please enter a sequence of positive integers: ").strip()
     try:
         test = int(example)
         if example[0] == '-':
             sys.exit("Sorry, positive integers only.")
     except ValueError:
         sys.exit("Sorry, badly formed input.")
+    #run it here
     print(",".join(decode_sequence(example)))
